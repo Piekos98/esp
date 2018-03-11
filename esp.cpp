b549764552696daa3594914453e0e1749c89be2b
Jaru66 <34154018+Jaru66@users.noreply.github.com>
2018-03-11 17:12:05 +0100

    Update esp.cpp
    zmieniłem położenie deklaracji "dodatnie" i uporządkowałem tworzenie obiektów 
    "
    OneWire oneWire(ONE_WIRE_BUS);
    DallasTemperature sensors(&oneWire);
    ESP8266WiFiMulti WiFiMulti;
    "

--- esp.cpp
+++ esp.cpp
@@ -1,51 +1,53 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
-ESP8266WiFiMulti WiFiMulti;
-
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#define ONE_WIRE_BUS 6
+#define ONE_WIRE_BUS D6
+
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+ESP8266WiFiMulti WiFiMulti;
+
+int dodania = 0;
 
 void setup() {
-  sensors.begin();
-  Serial.begin(115200);
-  WiFiMulti.addAP("Orange-E0B7", "32804607");
-  while ((WiFiMulti.run() != WL_CONNECTED)) {
-    Serial.print(".");
-    delay(200);
-  }
-  pinMode(5, INPUT_PULLUP);
+sensors.begin();
+Serial.begin(115200);
+WiFiMulti.addAP("NazwaSieciWiFi", "HasloDoSieciWifi");
+while ((WiFiMulti.run() != WL_CONNECTED)) {
+Serial.print(".");
+delay(200);
 }
-int dodania = 0;
+pinMode(D5, INPUT_PULLUP);
+}
+
+
 void loop() {
-  if (digitalRead(5) == LOW){
-    if ((WiFiMulti.run() == WL_CONNECTED)) {
-      dodania = dodania + 1;
-      sensors.requestTemperatures();
-      HTTPClient http;
-      String content = "http://smarthomeluna.cba.pl/add.php?a=";
-      content += sensors.getTempCByIndex(0);
+if (digitalRead(D5) == LOW){
+if ((WiFiMulti.run() == WL_CONNECTED)) {
+dodania = dodania + 1;
+sensors.requestTemperatures();
+HTTPClient http;
+String content = "http://strona.pl/add.php?a=";
+content += sensors.getTempCByIndex(0);
 
-      content += "&b=";
-      content += dodania;
+		content += "&b=";
+		content += dodania;
 
-      Serial.print("[HTTP] begin...\n");
-      http.begin(content);
-      int httpCode = http.GET();
-      if (httpCode > 0) {
-        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-        if (httpCode == HTTP_CODE_OK) {
-          String payload = http.getString();
-          Serial.println(payload);
-        }
-      }
-      else {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      }
-      http.end();
-    }
-  }
+		Serial.print("[HTTP] begin...\n");
+		http.begin(content);
+		int httpCode = http.GET();
+		if (httpCode > 0) {
+			Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+			if (httpCode == HTTP_CODE_OK) {
+				String payload = http.getString();
+				Serial.println(payload);
+			}
+		}
+		else {
+			Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+		}
+		http.end();
+	}
 }
